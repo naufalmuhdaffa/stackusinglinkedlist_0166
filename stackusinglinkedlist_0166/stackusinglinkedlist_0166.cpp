dc6c7e1922f64885c6881dee00beced3a3f9b5a6
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-05-11 10:09:54 +0700

    menghapus output

--- stackusinglinkedlist_0166/stackusinglinkedlist_0166.cpp
+++ stackusinglinkedlist_0166/stackusinglinkedlist_0166.cpp
@@ -79,7 +79,7 @@ int main()
         cout << "2. Pop\n";
         cout << "3. Peek\n";
         cout << "4. Exit\n";
-        cout << "Enter your choice: \n";
+        cout << "Enter your choice: ";
         cin >> choice;
 
         switch (choice)
@@ -105,6 +105,7 @@ int main()
                 cout << "Stack is empty. No top value" << endl;
             }
             break;
+
         case 4:
             cout << "Exiting program." << endl;
             break;
