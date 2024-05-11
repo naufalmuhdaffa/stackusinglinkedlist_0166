b819a6a3d016fcfc771b7a7ac7ad3f699f8f2ccf
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-05-11 09:41:25 +0700

    memberi akses public pd stack

--- stackusinglinkedlist_0166/stackusinglinkedlist_0166.cpp
+++ stackusinglinkedlist_0166/stackusinglinkedlist_0166.cpp
@@ -17,7 +17,10 @@ class Stack {
 private:
     Node* top; // Pointer to the top node of the stack
 
-
+public:
+    Stack() {
+        top = NULL; // Initialize the stack with a null top pointer
+    }
 };
 
 int main()
